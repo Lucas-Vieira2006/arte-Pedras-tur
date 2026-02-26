cc002c8d5543238523aa904c316a555c2b2cd19d
Lucas Vieira <lucasfoz2021@gmail.com>
2026-02-26 09:58:34 -0300

    Refactor: Usando variável de ambiente para API

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://artepedrastur.onrender.com', // Verifique se a porta é essa mesma
+  baseURL: import.meta.env.VITE_API_URL || 'https://artepedrastur.onrender.com', // Verifique se a porta é essa mesma
 });
 
 api.interceptors.request.use(async (config) => {
