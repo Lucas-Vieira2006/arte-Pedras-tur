3bc05adae4631eba2497b7cdbc010810b396c24f
Lucas Vieira <lucasfoz2021@gmail.com>
2026-02-26 10:07:20 -0300

    Fix: Case sensitivity de pastas 2

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
-import api from "../services/api";
+import api from "../services/Api.js";
 import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext();
